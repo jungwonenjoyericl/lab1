d6f050606e38fdcbda3107dab52fc16412978036
KameraSaki <adjosdaniel108@gmail.com>
2026-01-23 16:29:20 +0100

    update

---
+++ Volvo240.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class Volvo240 extends Car{
+
+    public Volvo240(){
+        nrDoors = 4;
+        color = Color.black;
+        enginePower = 100;
+        modelName = "Volvo240";
+        stopEngine();
+    }
+}
